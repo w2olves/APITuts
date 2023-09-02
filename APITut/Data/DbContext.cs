7d0a4b18aa5b40dd975ff333db3760087b95240c
Abhijit <w2olves@hotmail.com>
2023-09-01 20:20:36 -0400

    Efcore

---
+++ APITut/Data/DbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APITut.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+    }
+}
--- APITut/Models/Villa.cs
+++ APITut/Models/Villa.cs
@@ -1,9 +1,23 @@
-﻿namespace APITut.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APITut.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }    
+        public int Occupancy { get; set; }  
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+
+
+        public int SqFt { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
